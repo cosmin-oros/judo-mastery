99a5a4f13715a3e6c6e49d700773b9637c9b1e49
cosmin.oros <oroscosmin09@gmail.com>
2024-12-21 15:30:06 +0200

    i18n

--- judo-mastery/src/i18n/index.tsx
+++ judo-mastery/src/i18n/index.tsx
@@ -1,12 +1,22 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+
+// Importing translation files
 import translationEN from "../locales/en.json";
 import translationRO from "../locales/ro.json";
+import translationES from "../locales/es.json";
+import translationDE from "../locales/de.json";
+import translationFR from "../locales/fr.json";
+import translationJA from "../locales/ja.json";
 
 const resources = {
   "ro": { translation: translationRO },
   "en": { translation: translationEN },
+  "es": { translation: translationES },
+  "de": { translation: translationDE },
+  "fr": { translation: translationFR },
+  "ja": { translation: translationJA },
 };
 
 const initI18n = async () => {
