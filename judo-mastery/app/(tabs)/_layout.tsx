1db7e7ad072649fb60148cfbda5026343a6fdfc7
cosmin.oros <oroscosmin09@gmail.com>
2025-01-07 10:15:25 +0200

    tabs working

--- judo-mastery/app/(tabs)/_layout.tsx
+++ judo-mastery/app/(tabs)/_layout.tsx
@@ -1,55 +1,66 @@
-import { Tabs } from "expo-router";
+import { Stack, Tabs } from "expo-router";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
-import { StyleSheet } from "react-native";
 import { useTheme } from "@/src/theme/ThemeProvider";
+import { useTranslation } from "react-i18next";
+import { StyleSheet } from "react-native";
 
-export default function Layout() {
+const Layout = () => {
   const { theme } = useTheme();
+  const { t } = useTranslation();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarStyle: {
-          backgroundColor: theme.colors.primary,
-          height: 70,
-          paddingBottom: 10,
-          paddingTop: 5,
-          borderTopWidth: 1,
-          borderTopColor: theme.colors.text,
-        },
-        tabBarActiveTintColor: theme.colors.text,
-        tabBarInactiveTintColor: theme.colors.text,
-        tabBarShowLabel: true,
-        tabBarLabelStyle: styles.tabBarLabel,
-        headerShown: false,
-        tabBarIconStyle: {
-          marginBottom: -2,
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="home/index"
+    <>
+      <Stack.Screen
         options={{
-          title: "Home",
-          tabBarLabel: "Home",
-          tabBarIcon: ({ color, size }) => (
-            <FontAwesome name="home" size={size || 25} color={color} />
-          ),
+          headerShown: false,
+          gestureEnabled: false,
         }}
       />
-      <Tabs.Screen
-        name="profile/index"
-        options={{
-          title: "Profile",
-          tabBarLabel: "Profile",
-          tabBarIcon: ({ color, size }) => (
-            <FontAwesome name="user" size={size || 25} color={color} />
-          ),
+      <Tabs
+        initialRouteName="home"
+        screenOptions={{
+          tabBarStyle: {
+            backgroundColor: theme.colors.primary,
+            height: 70,
+            paddingBottom: 10,
+            paddingTop: 5,
+            borderTopWidth: 1,
+            borderTopColor: theme.colors.text,
+          },
+          tabBarActiveTintColor: theme.colors.text, 
+          tabBarInactiveTintColor: theme.colors.placeholder,
+          tabBarShowLabel: true,
+          tabBarLabelStyle: styles.tabBarLabel,
+          headerShown: false,
+          tabBarIconStyle: {
+            marginBottom: -2,
+          },
         }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: t("home.title"),
+            tabBarLabel: t("home.title"),
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome name="home" size={size || 25} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: t("profile.title"),
+            tabBarLabel: t("profile.title"),
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome name="user" size={size || 25} color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+    </>
   );
-}
+};
 
 const styles = StyleSheet.create({
   tabBarLabel: {
@@ -58,3 +69,5 @@ const styles = StyleSheet.create({
     textTransform: "capitalize",
   },
 });
+
+export default Layout;
