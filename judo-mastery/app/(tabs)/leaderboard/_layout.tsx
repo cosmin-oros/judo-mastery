ff5e1b36811eeb6e6981c4b35f1f39d6c7d39e5a
cosmin.oros <oroscosmin09@gmail.com>
2025-03-23 18:50:57 +0200

    fix

---
+++ judo-mastery/app/(tabs)/leaderboard/_layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Stack } from "expo-router";
+
+export default function Layout() {
+  return (
+    <>
+      <Stack screenOptions={{}}>
+        <Stack.Screen
+          name="index"
+          options={{
+            headerShown: false
+          }}
+        />
+      </Stack>
+    </>
+  );
+}
